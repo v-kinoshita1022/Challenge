296f451513cedc5d1898e8244b32cc62b6bf72f2
v-kinoshita1022 <v.kinoshita1022@gmail.com>
2016-04-26 20:50:12 +0900

    プログラミング基礎１課題

---
+++ kadai2.php
@@ -0,0 +1,34 @@
+<?php
+//課題１
+ echo 'Hello world.<br>';
+
+//課題２
+ echo "groove"."-"."gear<br>";
+
+//課題３
+ echo "木下優大です。<br>";
+ echo "スマホアプリを作れるようにしたいです。<br>";
+
+//課題４
+ const ASD = 2;
+ $i = 5;
+ echo ASD*$i+$i-ASD;
+
+//課題５
+ echo ASD+$i."<br>";
+ echo ASD-$i."<br>";
+ echo ASD*$i."<br>";
+ echo ASD/$i."<br>";
+
+//課題６
+if($i=1){
+  echo "1です!<br>";
+}else{
+  if($i=2){
+    echo "プログラミングキャンプ!<br>";
+  }elseif($i="a"){
+    echo "文字です！<br>";
+  }
+
+}
+ ?>
