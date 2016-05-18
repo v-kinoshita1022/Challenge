c5e1139909c7fc0f6bae55185dd32a2294d6e0ba
v-kinoshita1022 <v.kinoshita1022@gmail.com>
2016-05-18 11:25:35 +0900

    データ操作課題３ブラウザ側

---
+++ 5_3html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<html>
+  <head>
+    <title>コントロールサンプル</title>
+  </head>
+  <body>
+    <form action="5_4
+    kadai.php" method="post">
+      id<input type="text" name="id">
+      pass<input type="text" name="pass">
+      <input type="submit" name="btnSubmit">
+
+    </form>
+  </body>
+</html>
+
+<?php
+
+ ?>
