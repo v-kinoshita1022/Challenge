9ad8627a705b80531f68184155a6420f4cf0fb09
v-kinoshita1022 <v.kinoshita1022@gmail.com>
2016-05-18 11:28:31 +0900

    データ操作課題アップロードブラウザ側

---
+++ 5_5html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<html>
+  <head>
+    <title>コントロールサンプル</title>
+  </head>
+  <body>
+    <form enctype="multipart/form-data" action="5_5kadai.php" method="post">
+      ファイル選択:<input type="file" name="userfile"/>
+        <input type="submit" name="btnSubmit">
+
+    </form>
+  </body>
+</html>
+
+<?php
+
+
+ ?>
